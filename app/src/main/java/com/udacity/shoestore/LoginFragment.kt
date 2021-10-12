9736babd2ee06cbb2d480d350d301537905fdfed
Justin Casey <jcaseydev@gmail.com>
2021-10-12 09:02:31 -0700

    Added nav graph, login, Welcome and instruction Screens

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+        binding.loginButton.setOnClickListener{ view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+
+        binding.signupButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+        return binding.root
+    }
+}
